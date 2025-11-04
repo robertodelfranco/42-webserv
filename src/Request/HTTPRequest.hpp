81d2e489b263c3397d65525af1443108f91aebf5
Luis Frade <luis.carvalho.frade@gmail..com>
2025-11-04 20:37:43 -0300

    moved files of request domain to a new dir, called Request

--- src/Request/HTTPRequest.hpp
+++ src/Request/HTTPRequest.hpp
@@ -6,8 +6,26 @@
 /*   By: luide-ca <luide-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 09:58:08 by luide-ca          #+#    #+#             */
-/*   Updated: 2025/11/04 10:36:28 by luide-ca         ###   ########.fr       */
+/*   Updated: 2025/11/04 15:53:32 by luide-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-testando
+#ifndef HTTP_REQUEST_HPP
+# define HTTP_REQUEST_HPP
+
+# include <iostream>
+# include <exception>
+
+class HTTPRequest {
+	private:
+
+	public:
+			HTTPRequest(void);
+			HTTPRequest( /*feel with everything necessary*/ );
+			HTTPRequest( const HTTPRequest& other );
+			~HTTPRequest( /*feel with everything necessary*/ );
+			
+			HTTPRequest& operator=( const HTTPRequest& other );
+};
+
+#endif
